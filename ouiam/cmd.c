c4f4d61eba6ae081c1971907915900186b629eaf
oaoulad- <oaoulad-@c3r5p2.1337.ma>
2024-07-17 03:53:45 +0100

    commit

---
+++ ouiam/cmd.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (!(char)c)
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+t_token is_cmd(char *line, int *i, t_token my_token)
+{
+    int j;
+    t_token *new_token;
+
+    j = 0;
+    while (line[*i] && !ft(strchr("<>|$", line[*i])))
+        *i++;
+    new_token = malloc(sizeof(t_token));
+    if (!new_token)
+        return (NULL);
+    new_token->value = ft_substr(line, *i, *i - j);
+    new_token->type = "cmd";
+    new_token->next = NULL;
+    ft_add_token_back(&my_token, new_token);
+    return (new_token)
+}
