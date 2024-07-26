e2d07ae228a4213bc8c3dae8d46ec08669f36c9f
oaoulad- <oaoulad-@c3r6p1.1337.ma>
2024-07-26 10:38:53 +0100

    commit

---
+++ wiam/singl_qu.c
@@ -0,0 +1,15 @@
+#include "minishell.h"
+
+t_token *is_singl_qu(char *line, int *i, t_token *my_token)
+{
+    t_token *new_token;
+    new_token = malloc(sizeof(t_token));
+    if (!new_token)
+        return (NULL);
+    new_token->value = ft_substr(line, *i, 1);
+    new_token->type = "singl_qu";
+    new_token->next = NULL;
+    ft_add_token_back(&my_token, new_token);
+    (*i)++;
+    return (new_token);
+}
